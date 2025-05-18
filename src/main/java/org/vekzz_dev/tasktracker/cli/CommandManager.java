c13a71c62f5f3a7ce0d3fa0b65900edde178c2c9
vekzz-dev <194902316+vekzz-dev@users.noreply.github.com>
2025-05-18 15:54:08 -0600

    feat(cli): Registar comandos add, list, update, mark y delete

--- src/main/java/org/vekzz_dev/tasktracker/cli/CommandManager.java
+++ src/main/java/org/vekzz_dev/tasktracker/cli/CommandManager.java
@@ -1,4 +1,23 @@
 package org.vekzz_dev.tasktracker.cli;
 
+import org.vekzz_dev.tasktracker.controller.TaskController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
 public class CommandManager {
+    private static final Map<String, Consumer<String[]>> commands = new HashMap<>();
+
+    public static void main(String[] args) {
+        loadCommands();
+    }
+
+    private static void loadCommands() {
+        commands.put("add", TaskController::addTask);
+        commands.put("list", TaskController::listTask);
+        commands.put("update", TaskController::updateTask);
+        commands.put("mark", TaskController::markTask);
+        commands.put("delete", TaskController::deleteTask);
+    }
 }
