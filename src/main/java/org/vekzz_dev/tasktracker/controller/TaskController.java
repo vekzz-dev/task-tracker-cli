74c1bafff44569949d3e55e7d88733d6f33aa8bb
vekzz-dev <194902316+vekzz-dev@users.noreply.github.com>
2025-05-19 15:35:00 -0600

    style(controller): Agregar espaciado e identación entre métodos

--- src/main/java/org/vekzz_dev/tasktracker/controller/TaskController.java
+++ src/main/java/org/vekzz_dev/tasktracker/controller/TaskController.java
@@ -1,14 +1,18 @@
 package org.vekzz_dev.tasktracker.controller;
 
 public class TaskController {
-    public static void addTask(String[] args){
+    public static void addTask(String[] args) {
     }
-    public static void updateTask(String[] args){
+
+    public static void updateTask(String[] args) {
     }
-    public static void listTask(String[] args){
+
+    public static void listTask(String[] args) {
     }
-    public static void markTask(String[] args){
+
+    public static void markTask(String[] args) {
     }
-    public static void deleteTask(String[] args){
+
+    public static void deleteTask(String[] args) {
     }
 }
