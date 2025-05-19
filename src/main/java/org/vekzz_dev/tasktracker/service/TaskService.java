4bb5af4a05b5e12f1dd16bc7ab9f685d255caf24
vekzz-dev <194902316+vekzz-dev@users.noreply.github.com>
2025-05-19 16:49:09 -0600

    feat(service): Definir firma de métodos add, update, delete, mark y list

--- src/main/java/org/vekzz_dev/tasktracker/service/TaskService.java
+++ src/main/java/org/vekzz_dev/tasktracker/service/TaskService.java
@@ -1,4 +1,18 @@
 package org.vekzz_dev.tasktracker.service;
 
 public class TaskService {
+    public void add(String description) {
+    }
+
+    public void update(int id, String description) {
+    }
+
+    public void delete(int id) {
+    }
+
+    public void mark(int id, String status) {
+    }
+
+    public void list(String status) {
+    }
 }
