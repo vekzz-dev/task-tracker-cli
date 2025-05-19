de8c7490475fca3e4ffc5525824d82994a09d58b
vekzz-dev <194902316+vekzz-dev@users.noreply.github.com>
2025-05-19 15:23:15 -0600

    feat(model): Implementar clase Task como entidad base

---
+++ src/main/java/org/vekzz_dev/tasktracker/model/Task.java
@@ -0,0 +1,52 @@
+package org.vekzz_dev.tasktracker.model;
+
+import java.time.LocalDateTime;
+
+public class Task {
+    private final int id;
+    private String description;
+    private Status status;
+    private final LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Task(int id, String description, LocalDateTime createdAt, Status status, LocalDateTime updatedAt) {
+        this.id = id;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.status = status;
+        this.updatedAt = updatedAt;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return id +
+                " - desc='" + description + '\'' +
+                ", status=" + status +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt;
+    }
+}
