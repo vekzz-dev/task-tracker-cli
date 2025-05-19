c0d4c38bc681577868d52741c2d7894d1a7e2219
vekzz-dev <194902316+vekzz-dev@users.noreply.github.com>
2025-05-19 16:32:48 -0600

    feat(controller): Implementar lógica para validar la cantidad de argumentos

--- src/main/java/org/vekzz_dev/tasktracker/controller/TaskController.java
+++ src/main/java/org/vekzz_dev/tasktracker/controller/TaskController.java
@@ -1,6 +1,16 @@
 package org.vekzz_dev.tasktracker.controller;
 
+import java.util.Map;
+
 public class TaskController {
+    private static final Map<String, int[]> expectedArgsByCommand = Map.of(
+            "add", new int[]{2, 2},
+            "update", new int[]{2, 2},
+            "delete", new int[]{1, 1},
+            "mark", new int[]{2, 2},
+            "list", new int[]{0, 1}
+    );
+
     public static void addTask(String[] args) {
     }
 
@@ -14,5 +24,21 @@ public class TaskController {
     }
 
     public static void deleteTask(String[] args) {
+    }
+
+    public static boolean isNumberOfArgsCorrect(String[] args, String action) {
+        int[] range = expectedArgsByCommand.get(action);
+        int argCount = args.length;
+        int min = range[0];
+        int max = range[1];
+        if (range == null) throw new IllegalStateException("Acción desconocida: " + action);
+        if (argCount < min || argCount > max) {
+            String rangeMsg = (min == max)
+                    ? min + " argumento(s)"
+                    : "entre " + min + " y " + max + " argumento(s)";
+            throw new IllegalArgumentException("El comando '" + action + "' requiere " +
+                    rangeMsg + " , pero se recibieron " + args.length + ".");
+        }
+        return true;
     }
 }
