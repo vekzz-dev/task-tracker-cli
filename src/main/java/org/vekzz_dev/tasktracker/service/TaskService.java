680c26509a9723458bef8b87ca14df826b18d1b1
vekzz-dev <194902316+vekzz-dev@users.noreply.github.com>
2025-05-20 21:21:31 -0600

    feat(service): Implementar lógica para add, update, delete, mark y list en clase TaskService

--- src/main/java/org/vekzz_dev/tasktracker/service/TaskService.java
+++ src/main/java/org/vekzz_dev/tasktracker/service/TaskService.java
@@ -22,24 +22,47 @@ public class TaskService {
                 .orElseGet(ArrayList::new);
     }
 
-    public void add(String description) {
+    public int add(String description) {
         int id = idRepository.nextId();
         Status status = Status.IN_PROGRESS;
         LocalDateTime createdAndUpdatedAt = LocalDateTime.now();
         Task task = new Task(id, description, status, createdAndUpdatedAt, createdAndUpdatedAt);
         taskList.add(task);
         taskRepository.save(taskList);
+        return id;
     }
 
     public void update(int id, String description) {
+        Task task = findById(id);
+        task.setDescription(description);
+        task.setUpdatedAt(LocalDateTime.now());
+        taskRepository.save(taskList);
     }
 
     public void delete(int id) {
+        Task task = findById(id);
+        taskList.remove(task);
+        taskRepository.save(taskList);
+    }
+
+    private Task findById(int id) {
+        return taskList.stream()
+                .filter(t -> t.getId() == id)
+                .findAny()
+                .orElseThrow(() -> new RuntimeException("No se encontró la tarea con id: " + id));
     }
 
     public void mark(int id, String status) {
+        Task task = findById(id);
+        task.setStatus(Status.valueOf(status));
+        taskRepository.save(taskList);
     }
 
-    public void list(String status) {
+    public List<Task> list(String status) {
+        List<Task> allTask = taskRepository.findAll();
+        if (status == null || status.isEmpty()) return allTask;
+        return allTask.stream()
+                .filter(t -> t.getStatus() == Status.valueOf(status))
+                .toList();
     }
 }
