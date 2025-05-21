1c9fc7c291c9b53a8f5e00502d4f9272f1dafcbd
vekzz-dev <194902316+vekzz-dev@users.noreply.github.com>
2025-05-21 16:52:57 -0600

    add(model): Agregar descripciones a los enums de Status

--- src/main/java/org/vekzz_dev/tasktracker/model/Status.java
+++ src/main/java/org/vekzz_dev/tasktracker/model/Status.java
@@ -1,7 +1,17 @@
 package org.vekzz_dev.tasktracker.model;
 
 public enum Status {
-    DONE,
-    TODO,
-    IN_PROGRESS
+    DONE("hecho ✅"),
+    TODO("pendiente 🟦"),
+    IN_PROGRESS("en progreso 🟠");
+
+    private final String description;
+
+    Status(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
