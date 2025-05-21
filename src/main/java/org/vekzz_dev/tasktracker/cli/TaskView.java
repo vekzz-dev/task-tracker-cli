5ad13272f7be243e38a18f591cd4b21617a70ae1
vekzz-dev <194902316+vekzz-dev@users.noreply.github.com>
2025-05-20 21:18:21 -0600

    feat(cli): Implementar clase TaskView para la impresión de tareas en consola

---
+++ src/main/java/org/vekzz_dev/tasktracker/cli/TaskView.java
@@ -0,0 +1,19 @@
+package org.vekzz_dev.tasktracker.cli;
+
+import org.vekzz_dev.tasktracker.model.Task;
+
+import java.util.List;
+
+public class TaskView {
+    public void showTasks(List<Task> tasks) {
+        if (tasks.isEmpty()) {
+            System.out.println("No hay tareas.");
+        } else {
+            tasks.forEach(System.out::println);
+        }
+    }
+
+    public void showMessage(String message) {
+        System.out.println(message);
+    }
+}
