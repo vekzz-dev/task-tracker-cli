01c0ef6469f26156429222867b835073ee38be18
vekzz-dev <194902316+vekzz-dev@users.noreply.github.com>
2026-01-20 10:26:05 -0600

    refactor(main): move Main class to correct package location

---
+++ src/main/java/io/vekzz_dev/task_tracker/Main.java
@@ -0,0 +1,24 @@
+package io.vekzz_dev.task_tracker;
+
+import io.vekzz_dev.task_tracker.cli.CommandRouter;
+import io.vekzz_dev.task_tracker.models.TaskStore;
+import io.vekzz_dev.task_tracker.repositories.Repository;
+import io.vekzz_dev.task_tracker.repositories.TaskRepository;
+import io.vekzz_dev.task_tracker.services.TaskService;
+import io.vekzz_dev.task_tracker.storage.FileManager;
+import io.vekzz_dev.task_tracker.utils.JacksonConfig;
+import io.vekzz_dev.task_tracker.utils.TaskServiceHolder;
+
+public class Main {
+
+    static void main(String[] args) {
+        FileManager fileManager = new FileManager();
+        Repository<TaskStore> repository = new TaskRepository(JacksonConfig.createMapper(), fileManager);
+
+        TaskService service = new TaskService(repository);
+        TaskServiceHolder.setTaskService(service);
+
+        CommandRouter router = new CommandRouter(args);
+        router.process();
+    }
+}
