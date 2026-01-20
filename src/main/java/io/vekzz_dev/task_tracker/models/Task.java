718a6b43e4b31afa83324a0eb6b11b46004e1ea2
vekzz-dev <194902316+vekzz-dev@users.noreply.github.com>
2026-01-20 09:59:34 -0600

    feat(models): create task entity with ID, description, status and timestamps

---
+++ src/main/java/io/vekzz_dev/task_tracker/models/Task.java
@@ -0,0 +1,55 @@
+package io.vekzz_dev.task_tracker.models;
+
+import java.time.LocalDateTime;
+
+public class Task {
+
+    private int id;
+    private String description;
+    private Status status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Task() {
+    }
+
+    public Task(int id, String description, Status status, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.description = description;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
