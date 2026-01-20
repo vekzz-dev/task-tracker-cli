db1eab95bd077f9ecfbd0b267e9d61a8bd3ffa10
vekzz-dev <194902316+vekzz-dev@users.noreply.github.com>
2026-01-20 10:01:09 -0600

    feat(cli): add command to list all tasks or filter by status

---
+++ src/main/java/io/vekzz_dev/task_tracker/cli/commands/ListCommand.java
@@ -0,0 +1,40 @@
+package io.vekzz_dev.task_tracker.cli.commands;
+
+import io.vekzz_dev.task_tracker.cli.Command;
+import io.vekzz_dev.task_tracker.models.Task;
+import io.vekzz_dev.task_tracker.output.OutputPrinter;
+import io.vekzz_dev.task_tracker.services.TaskService;
+import io.vekzz_dev.task_tracker.utils.TaskServiceHolder;
+import io.vekzz_dev.task_tracker.validations.ArgumentValidator;
+
+import java.util.List;
+
+public class ListCommand implements Command {
+
+    private final List<String> arguments;
+    private final TaskService taskService;
+
+    public ListCommand(List<String> arguments) {
+        this.arguments = arguments;
+        this.taskService = TaskServiceHolder.getTaskService();
+    }
+
+    @Override
+    public void execute() {
+        try {
+            ArgumentValidator.validateArgumentSize(arguments, 1,
+                    "A status is the only required argument");
+
+            String status = arguments.isEmpty() ? "all" : arguments.getFirst();
+
+            List<Task> tasks = taskService.list(status);
+
+            if (tasks.isEmpty()) throw new IllegalArgumentException("No tasks found");
+
+            OutputPrinter.printTable(tasks);
+
+        } catch (Exception e) {
+            OutputPrinter.printMessage(e.getMessage());
+        }
+    }
+}
