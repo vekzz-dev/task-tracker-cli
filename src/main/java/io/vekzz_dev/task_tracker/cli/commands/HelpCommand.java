b4a9857dd8600b0d270f0cb90f5b2d9af273d7a6
vekzz-dev <194902316+vekzz-dev@users.noreply.github.com>
2026-01-20 10:01:24 -0600

    feat(cli): add command to display usage information

---
+++ src/main/java/io/vekzz_dev/task_tracker/cli/commands/HelpCommand.java
@@ -0,0 +1,27 @@
+package io.vekzz_dev.task_tracker.cli.commands;
+
+import io.vekzz_dev.task_tracker.cli.Command;
+import io.vekzz_dev.task_tracker.output.OutputPrinter;
+
+public class HelpCommand implements Command {
+
+    @Override
+    public void execute() {
+        try {
+            OutputPrinter.printMessage("""
+                    Available commands:
+                    add <description>               -> Add new task.
+                    update <id> <new description>   -> Update existing task.
+                    delete <id>                     -> Delete task.
+                    list <status>                   -> List all tasks or by status (done, todo, in-progress).
+                    mark <id> <status>              -> Mark task (done, todo, in-progress).
+                    help                            -> View command information.
+                    exit                            -> Exit.
+                    """
+            );
+
+        } catch (Exception e) {
+            OutputPrinter.printMessage(e.getMessage());
+        }
+    }
+}
