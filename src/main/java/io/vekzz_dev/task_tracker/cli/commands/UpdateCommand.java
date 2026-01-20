1740e2435c582d6a6d1bc7f51da7117fe10b153c
vekzz-dev <194902316+vekzz-dev@users.noreply.github.com>
2026-01-20 10:01:14 -0600

    feat(cli): add command to update task description

---
+++ src/main/java/io/vekzz_dev/task_tracker/cli/commands/UpdateCommand.java
@@ -0,0 +1,39 @@
+package io.vekzz_dev.task_tracker.cli.commands;
+
+import io.vekzz_dev.task_tracker.cli.Command;
+import io.vekzz_dev.task_tracker.output.OutputPrinter;
+import io.vekzz_dev.task_tracker.services.TaskService;
+import io.vekzz_dev.task_tracker.utils.TaskServiceHolder;
+import io.vekzz_dev.task_tracker.validations.ArgumentValidator;
+import io.vekzz_dev.task_tracker.validations.TaskValidator;
+
+import java.util.List;
+
+public class UpdateCommand implements Command {
+
+    private final List<String> arguments;
+    private final TaskService taskService;
+
+    public UpdateCommand(List<String> arguments) {
+        this.arguments = arguments;
+        taskService = TaskServiceHolder.getTaskService();
+    }
+
+    @Override
+    public void execute() {
+        try {
+            ArgumentValidator.validateArgumentSize(arguments, 2,
+                    "ID and description are the only required.");
+
+            int id = TaskValidator.parseAndValidateId(arguments.getFirst());
+            String description = arguments.get(1);
+            TaskValidator.validateDescription(description);
+
+            taskService.update(id, description);
+            OutputPrinter.printMessage("Task updated successfully with ID: " + id);
+
+        } catch (Exception e) {
+            OutputPrinter.printMessage(e.getMessage());
+        }
+    }
+}
