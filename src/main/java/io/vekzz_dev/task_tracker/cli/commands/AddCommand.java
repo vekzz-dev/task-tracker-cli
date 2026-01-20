aee7536e629c71d65b56853cf854474ca9f60ddf
vekzz-dev <194902316+vekzz-dev@users.noreply.github.com>
2026-01-20 10:01:08 -0600

    feat(cli): add command to create new tasks with description

---
+++ src/main/java/io/vekzz_dev/task_tracker/cli/commands/AddCommand.java
@@ -0,0 +1,38 @@
+package io.vekzz_dev.task_tracker.cli.commands;
+
+import io.vekzz_dev.task_tracker.cli.Command;
+import io.vekzz_dev.task_tracker.output.OutputPrinter;
+import io.vekzz_dev.task_tracker.services.TaskService;
+import io.vekzz_dev.task_tracker.utils.TaskServiceHolder;
+import io.vekzz_dev.task_tracker.validations.ArgumentValidator;
+import io.vekzz_dev.task_tracker.validations.TaskValidator;
+
+import java.util.List;
+
+public class AddCommand implements Command {
+
+    private final List<String> arguments;
+    private final TaskService taskService;
+
+    public AddCommand(List<String> arguments) {
+        this.arguments = arguments;
+        this.taskService = TaskServiceHolder.getTaskService();
+    }
+
+    @Override
+    public void execute() {
+        try {
+            ArgumentValidator.validateArgumentSize(arguments, 1,
+                    "A description is the only required.");
+
+            String description = arguments.getFirst();
+            TaskValidator.validateDescription(description);
+
+            int id = taskService.add(description);
+            OutputPrinter.printMessage("Task added successfully. ID: " + id);
+
+        } catch (Exception e) {
+            OutputPrinter.printMessage(e.getMessage());
+        }
+    }
+}
